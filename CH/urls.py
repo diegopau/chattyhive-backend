38998c83411c884e614faf241b5794670ed5e341
lorgaco <lorenzo.gaco@gmail.com>
2013-10-09 18:50:23 +0200

    DB created

--- CH/urls.py
+++ CH/urls.py
@@ -2,5 +2,5 @@ __author__ = 'xurxo'
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', '{{ project_name }}.views.home', name='home'),
+    url(r'^$', 'CH.views.index', name='home'),
 )
---
+++ CH/views.py
@@ -0,0 +1,5 @@
+__author__ = 'lorenzo'
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the poll index.")
