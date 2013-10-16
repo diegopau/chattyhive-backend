55848ab3a9799775d107d7476e480b5b130c810c
lorgaco <lorenzo.gaco@gmail.com>
2013-10-16 18:12:06 +0200

    chat app created

--- CH/urls.py
+++ CH/urls.py
@@ -2,5 +2,6 @@ __author__ = 'xurxo'
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'chat_app.views.login', name='home'),
+    url(r'^$', 'chat_app.views.login', name='login'),
+    url(r'^chat/', 'chat_app.views.chat', name='chat')
 )
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,15 +1,19 @@
 __author__ = 'lorenzo'
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from chat_app.models import *
 
 def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        return HttpResponseRedirect("bienvenido")
+        return HttpResponseRedirect("/chat/")
     else:
         form = LoginForm()
     #return HttpResponse("Hello, world. You're at the poll index.")
     return render(request, "chat_app/login.html", {
         'form':form
     })
+
+def chat(request):
+    return render_to_response('chat_app/chat.html')
