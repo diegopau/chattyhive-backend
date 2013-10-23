3b7b0d9f04d48ed159e15caf195d88652d54fe13
lorgaco <lorenzo.gaco@gmail.com>
2013-10-23 18:57:23 +0200

    trying triggering

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,8 +1,8 @@
 __author__ = 'lorenzo'
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.http import HttpResponse
 from chat_app.models import *
+import pusher
 
 def login(request):
     if request.method == 'POST':
@@ -20,12 +20,24 @@ def chat(request):
     user=request.session['user']
     app_key="55129"
     key='f073ebb6f5d1b918e59e'
+    secret ='360b346d88ee47d4c230'
     channel='public_test'
     event='msg'
-    return render(request, "chat_app/chat.html", {
-        'user': user,
-        'app_key': app_key,
-        'key': key,
-        'channel' : channel,
-        'event' : event
-    })
+    if request.method == 'POST':
+        form = forms.Form(request.POST)
+        msg=form.cleaned_data['value']
+        p = pusher.Pusher(
+            app_id=app_key,
+            key=key,
+            secret=secret
+        )
+        p[channel].trigger(event, {"user":user,"msg":msg})
+
+    else:
+        return render(request, "chat_app/chat.html", {
+            'user': user,
+            'app_key': app_key,
+            'key': key,
+            'channel' : channel,
+            'event' : event
+        })
