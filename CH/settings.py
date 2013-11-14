785f13eeff5b92dfb9daf2132703c11d3ca2104b
xulegaspi <xulegaspi@gmail.com>
2013-11-14 16:50:05 +0100

    changes for heroku upload.

--- CH/settings.py
+++ CH/settings.py
@@ -164,7 +164,7 @@ LOGGING = {
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 #DATABASES['default'] =  dj_database_url.config()
-DATABASES = {'default': dj_database_url.config(default='postgres://chattytestuser:chattytestpass@localhost/chattytestdbdb')}
+DATABASES = {'default': dj_database_url.config(default='postgres://chattytestuser:chattytestpass@localhost/chattytestdb')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
