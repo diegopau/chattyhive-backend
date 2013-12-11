0f3e16dea73383bd02c0ba0f4920acf59c735087
lorgaco <lorenzo.gaco@gmail.com>
2013-12-11 17:34:31 +0100

    added user models

--- login/models.py
+++ login/models.py
@@ -1,1 +1,50 @@
 __author__ = 'lorenzo'
+
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+
+
+class ChUserManager(UserManager):
+    def create_user(self, username, email, password):
+        user = self.model(username=username)
+        user.email = email
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(username, email, password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class ChUser(AbstractUser):
+    ##****************Key Fields****************#
+    # username = AbstractUser
+    # email = AbstractUser
+    #****************Info Fields****************#
+    # first_name = AbstractUser
+    # last_name = AbstractUser
+    birth_date = models.DateField(null=True, auto_now=False, auto_now_add=False)
+    # sex =
+    # language =
+    # timezone =
+    #****************Control Fields****************#
+    is_authenticated = models.BooleanField(default=False)
+
+    objects = ChUserManager()
+
+    # REQUIRED_FIELDS = ['email', 'Name', 'LastName']
+    USERNAME_FIELD = 'username'
+
+    # def get_full_name(self):
+    #     return "% % %"(self.username)
+
+    # def get_short_name(self):
+    #     return "% % %"(self.username)
+
+    def is_authenticated(self):
+        return AbstractUser.is_authenticated(self)
