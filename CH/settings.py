639ba5a1f77b1501469a346173ce0a8e2790f78b
lorgaco <lorenzo.gaco@gmail.com>
2014-01-21 16:01:19 +0100

    More functions in the social auth pipeline working without database errors

--- CH/settings.py
+++ CH/settings.py
@@ -153,8 +153,8 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.social_details',
     'social.pipeline.social_auth.social_uid',
     'social.pipeline.social_auth.auth_allowed',
-    # 'social.pipeline.social_auth.social_user', #TODO database error?
-    # 'social.pipeline.social_auth.associate_by_email',
+    'social.pipeline.social_auth.social_user', #TODO database error?
+    'social.pipeline.social_auth.associate_by_email',
     'social.pipeline.user.get_username',
     'social.pipeline.user.create_user',
     'social.pipeline.social_auth.associate_user',
