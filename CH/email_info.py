f08d1e835a26c60a87590ab7d6d71483eebf766a
xulegaspi <xulegaspi@gmail.com>
2014-04-02 18:36:44 +0200

    Email settings updated.

--- CH/email_info.py
+++ CH/email_info.py
@@ -2,6 +2,6 @@ __author__ = 'xurxo'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'app19648184@heroku.com'
-EMAIL_HOST_PASSWORD = 'g7u229xd'
+EMAIL_HOST_PASSWORD = 'g6vltdac'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
