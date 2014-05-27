753df73318a9219e3877f894150304984478332a
lorgaco <lorenzo.gaco@gmail.com>
2014-05-27 18:29:17 +0200

    Automatic database inserting added for categories. Only for 2 categories, work in progress.

---
+++ core/sql/chcategory.sql
@@ -0,0 +1,4 @@
+INSERT INTO core_chcategory ("name", "group", "description")
+  VALUES ('Aficiones y ocio - general', 'Aficiones y ocio', 'Dummy description');
+INSERT INTO core_chcategory ("name", "group", "description")
+  VALUES ('Aprender idiomas', 'Aficiones y ocio', 'Dummy description');
