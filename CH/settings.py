81aca993253a8a49eb68fe8d2a25dc84d04289b2
xulegaspi <xulegaspi@gmail.com>
2014-05-28 19:37:15 +0200

    XsSaring Middleware added for Acces-Control-Allow-Origin and AngularJS compatibility.

--- CH/settings.py
+++ CH/settings.py
@@ -104,6 +104,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'CH.django-crossdomainxhr-middleware.XsSharing',
     # Uncomment the next line for simple click jacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
