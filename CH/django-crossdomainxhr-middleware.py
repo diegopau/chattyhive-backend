02a36df6e0d306032eb00a2db30a0644b5615a62
xulegaspi <xulegaspi@gmail.com>
2014-06-17 18:53:11 +0200

    OPTIONS accepted for angularjs and browser behaviour.

--- CH/django-crossdomainxhr-middleware.py
+++ CH/django-crossdomainxhr-middleware.py
@@ -36,7 +36,7 @@ class XsSharing(object):
                 # request['Origin']
 
                 print("OPTION METHOD - AngularJS - Browser")
-                response['Access-Control-Allow-Origin'] = request.META['Origin']
+                response['Access-Control-Allow-Origin'] = request['Origin']
                 response['Access-Control-Allow-Credentials'] = 'true'
                 response['Access-Control-Allow-Methods'] = ['POST', 'GET', 'OPTIONS']
                 response['Access-Control-Allow-Headers'] = '*'
