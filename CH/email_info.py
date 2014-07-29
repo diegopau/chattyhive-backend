770f0ac71c805595bc51bd8f5282f3099174b694
xulegaspi <xulegaspi@gmail.com>
2014-07-29 19:52:27 +0200

    Sendgrid set up

--- CH/email_info.py
+++ CH/email_info.py
@@ -1,7 +1,7 @@
 __author__ = 'xurxo'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'app19648184@heroku.com'
-EMAIL_HOST_PASSWORD = 'g6vltdac'
+EMAIL_HOST_USER = 'app27882600@heroku.com'
+EMAIL_HOST_PASSWORD = '8r3ni4mp'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
