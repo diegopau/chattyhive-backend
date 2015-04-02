7bb8757c18817dfb4ac2c32e30455e771f8e14c2
Diego Ocampo <diegoocampo8@gmail.com>
2015-04-02 11:28:15 +0200

    Testing: more work on random generators

--- core/test/get_random_stuff.py
+++ core/test/get_random_stuff.py
@@ -1,6 +1,12 @@
 from core.test.text_generator.random_generator import Generator
-from core.test.name_generator.names import
-from random import randint
+from core.test.name_generator import names
+from random import randint, random
+import string
+
+
+##########################################################
+# THIS IS FOR RANDOM BIG TEXT (random_generator package) #
+##########################################################
 
 with open('data/sample.txt', 'r') as sample_txt:
     sample = sample_txt.read()
@@ -8,9 +14,15 @@ with open('data/dictionary.txt', 'r') as dictionary_txt:
     dictionary = dictionary_txt.read().split()
 
 generator = Generator(sample, dictionary)
+##########################################################
+
+
+def string_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 def get_random_tag():
-
+    return string_generator(size=randint(1, 32), chars=string.ascii_uppercase + string.ascii_lowercase + '_')
 
 
 # def get_random_hive_name():
@@ -23,6 +35,10 @@ def get_random_tag():
 #
 #
 # def get_random_category_code():
+
+
+def get_random_full_name(gender):
+    names.getfull_name(gender)
 
 
 def get_random_hive_description():
